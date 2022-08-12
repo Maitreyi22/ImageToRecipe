62a0e9ced9c69284d80f1c70acdcfb2a42fee53f
Maitreyi <maitreyikalantre@gmail.com>
2022-08-12 15:43:13 +0530

    Breaking of lines

--- backend/app.py
+++ backend/app.py
@@ -29,7 +29,7 @@ category={
     6: ['dhokla','Khaman Dhokla'], 7: ['fried_rice','Fried Rice'], 8: ['idli','Idli'], 9: ['jalegi','Jalebi'],
     10: ['kathi_rolls','Kaathi Rolls'], 11: ['kadai_paneer','Kadai Paneer'], 12: ['kulfi','Kulfi'],
     13: ['masala_dosa','Masala Dosa'], 14: ['momos','Veg Momos'], 15: ['paani_puri','Paani Puri'],
-    16: ['pakode','Pakode'], 17: ['pav_bhaji','Pav Bhaji'], 18: ['pizza','Pizza'], 19: ['samosa','Samosa']
+    16: ['pakode','Pakode'], 17: ['pav_bhaji','Pav Bhaji'], 18: ['pizza','Veg Pizza'], 19: ['samosa','Samosa']
 }
 
 def predict_image(filename,model):
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -37,7 +37,49 @@ class _DashBoardState extends State<DashBoard> {
     var request = http.MultipartRequest(
       'POST',
 
-      Uri.parse('https://d43f-124-66-170-204.in.ngrok.io/predict'),
+      Uri.parse('https://a8fa-124-66-170-219.in.ngrok.io/predict'),
+
+      // Uri.parse('http://10.0.2.2:5001/predict'),
+    );
+    Map<String, String> headers = {"Content-type": "multipart/form-data"};
+    request.files.add(
+      http.MultipartFile(
+        'image',
+        selectedImage!.readAsBytes().asStream(),
+        selectedImage!.lengthSync(),
+        filename: selectedImage!.path.split('/').last,
+      ),
+    );
+    request.headers.addAll(headers);
+    print("request: $request");
+    final res = await request.send();
+    http.Response response = await http.Response.fromStream(res);
+    final resJson = jsonDecode(response.body);
+
+    name = resJson["name"];
+
+    setState(() {});
+
+    setState(() {
+      name = resJson["name"];
+    });
+
+    // ignore: use_build_context_synchronously
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return ReciepePage(
+        predictedName: name!,
+      );
+    }));
+  }
+
+  onCameraImage(source) async {
+    var image = await ImagePicker()
+        .pickImage(imageQuality: 90, source: ImageSource.camera);
+    selectedImage = File(image!.path);
+    var request = http.MultipartRequest(
+      'POST',
+
+      Uri.parse('https://a8fa-124-66-170-219.in.ngrok.io/predict'),
 
       // Uri.parse('http://10.0.2.2:5001/predict'),
     );
@@ -288,7 +330,7 @@ class _DashBoardState extends State<DashBoard> {
                 height: 60,
                 child: InkWell(
                   onTap: () {
-                    onUploadImage(ImageSource.camera);
+                    onCameraImage(ImageSource.camera);
                   },
                   child: Card(
                     elevation: 0,
